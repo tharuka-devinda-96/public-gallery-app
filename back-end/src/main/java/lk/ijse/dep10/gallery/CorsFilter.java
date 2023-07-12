690168986f985fe54948047eb7d187997f0f709c
tharuka-devinda <tharukadevinda96@gmail.com>
2023-07-13 04:14:06 +0530

    Download done

---
+++ back-end/src/main/java/lk/ijse/dep10/gallery/CorsFilter.java
@@ -0,0 +1,2 @@
+package lk.ijse.dep10.gallery;public class CorsFilter {
+}
--- front-end/src/js/app.js
+++ front-end/src/js/app.js
@@ -1,4 +1,4 @@
-import {save} from 'file-saver';
+import {saveAs} from 'file-saver';
 
 const overlay = $("#overlay");
 const btnUpload = $("#btn-upload");
@@ -75,19 +75,13 @@ function loadAllImages() {
             divElm.css('background-image', `url(${imageUrl})`);
 
             // Add download icon to the image
-            const downloadIcon = $('<button type="button" class="btn btn-light"><svg width="40px" height="40px" viewBox="-2.4 -2.4 28.80 28.80" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
-                '\n' +
-                '<g id="SVGRepo_bgCarrier" stroke-width="0">\n' +
-                '\n' +
-                '<rect x="-2.4" y="-2.4" width="28.80" height="28.80" rx="14.4" fill="#7ed0ec"/>\n' +
-                '\n' +
-                '</g>\n' +
-                '\n' +
-                '<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>\n' +
-                '\n' +
-                '<g id="SVGRepo_iconCarrier"> <path d="M3 15C3 17.8284 3 19.2426 3.87868 20.1213C4.75736 21 6.17157 21 9 21H15C17.8284 21 19.2426 21 20.1213 20.1213C21 19.2426 21 17.8284 21 15" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> <path d="M12 3V16M12 16L16 11.625M12 16L8 11.625" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> </g>\n' +
-                '\n' +
-                '</svg></button>');
+            const downloadIcon = $(`<button type="button" class="btn btn-light">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor"
+                                     className="bi bi-cloud-arrow-down-fill" class="download-icon" viewBox="0 0 16 16">
+                                    <path class="download-icon"
+                                        d="M8 2a5.53 5.53 0 0 0-3.594 1.342c-.766.66-1.321 1.52-1.464 2.383C1.266 6.095 0 7.555 0 9.318 0 11.366 1.708 13 3.781 13h8.906C14.502 13 16 11.57 16 9.773c0-1.636-1.242-2.969-2.834-3.194C12.923 3.999 10.69 2 8 2zm2.354 6.854-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 9.293V5.5a.5.5 0 0 1 1 0v3.793l1.146-1.147a.5.5 0 0 1 .708.708z"/>
+                                    </svg>
+            </button>`);
 
             downloadIcon.addClass('download-button');
             downloadIcon.addClass('download-icon');
@@ -105,6 +99,6 @@ mainElm.on('click', (evt)=> {
     if((evt?.target.classList.contains("download-icon"))){
         const downUrl = $(evt.target).parents('div').css('background-image').replace('url("', '').replace('")', '');;
         const imageName = downUrl.replace(`${REST_API_URL}/images/`, "");
-        save(downUrl, imageName);
+        saveAs(downUrl, imageName);
     }
 });
